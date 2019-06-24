2a8d1c629312604e438809733d9ac40d6f7f8443
Ucok Freddy <ucokfm@gmail.com>
2019-06-24 14:02:45 +0700

    Convert callback to async await style

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const util = require('util');
 const express = require('express');
 const app = express();
 
@@ -7,9 +8,19 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
 module.exports = (db) => {
+    const dbAll = (util.promisify(db.all)).bind(db);
+    const dbRun = (sql, params) => {
+        return new Promise((resolve, reject) => {
+            db.run(sql, params, function (err) {
+                if (err) reject(err);
+                resolve(this.lastID);
+            });
+        });
+    };
+
     app.get('/health', (req, res) => res.send('Healthy'));
 
-    app.post('/rides', jsonParser, (req, res) => {
+    app.post('/rides', jsonParser, async (req, res) => {
         const startLatitude = Number(req.body.start_lat);
         const startLongitude = Number(req.body.start_long);
         const endLatitude = Number(req.body.end_lat);
@@ -55,28 +66,19 @@ module.exports = (db) => {
 
         var values = [req.body.start_lat, req.body.start_long, req.body.end_lat, req.body.end_long, req.body.rider_name, req.body.driver_name, req.body.driver_vehicle];
         
-        db.run('INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (?, ?, ?, ?, ?, ?, ?)', values, function (err) {
-            if (err) {
-                return res.send({
-                    error_code: 'SERVER_ERROR',
-                    message: 'Unknown error'
-                });
-            }
-
-            db.all('SELECT * FROM Rides WHERE rideID = ?', this.lastID, function (err, rows) {
-                if (err) {
-                    return res.send({
-                        error_code: 'SERVER_ERROR',
-                        message: 'Unknown error'
-                    });
-                }
-
-                res.send(rows);
-            });
-        });
+        try {
+            const lastID = await dbRun('INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (?, ?, ?, ?, ?, ?, ?)', values);
+            const rows = await dbAll('SELECT * FROM Rides WHERE rideID = ?', lastID);
+            res.send(rows);
+        } catch (error) {
+            return res.send({
+                error_code: 'SERVER_ERROR',
+                message: 'Unknown error'
+            }); 
+        }
     });
 
-    app.get('/rides', (req, res) => {
+    app.get('/rides', async (req, res) => {
         const page = Number(req.query.page);
         const limit = req.query.limit ? Number(req.query.limit) : 10;
         let sql = 'SELECT * FROM Rides';
@@ -86,43 +88,39 @@ module.exports = (db) => {
             const offset = (page - 1) * limit;
             params = [ limit, offset ];
         }
-        db.all(sql, params, function (err, rows) {
-            if (err) {
-                return res.send({
-                    error_code: 'SERVER_ERROR',
-                    message: 'Unknown error'
-                });
-            }
-
+        try {
+            const rows = await dbAll(sql, params);
             if (rows.length === 0) {
                 return res.send({
                     error_code: 'RIDES_NOT_FOUND_ERROR',
                     message: 'Could not find any rides'
                 });
             }
-
             res.send(rows);
-        });
+        } catch (err) {
+            return res.send({
+                error_code: 'SERVER_ERROR',
+                message: 'Unknown error'
+            });
+        }
     });
 
-    app.get('/rides/:id', (req, res) => {
-        db.all(`SELECT * FROM Rides WHERE rideID='${req.params.id}'`, function (err, rows) {
-            if (err) {
-                return res.send({
-                    error_code: 'SERVER_ERROR',
-                    message: 'Unknown error'
-                });
-            }
-
+    app.get('/rides/:id', async (req, res) => {
+        try {
+            const rows = await dbAll(`SELECT * FROM Rides WHERE rideID='${req.params.id}'`);
             if (rows.length === 0) {
                 return res.send({
                     error_code: 'RIDES_NOT_FOUND_ERROR',
                     message: 'Could not find any rides'
                 });
             }
-
             res.send(rows);
-        });
+        } catch (err) {
+            return res.send({
+                error_code: 'SERVER_ERROR',
+                message: 'Unknown error'
+            }); 
+        }
     });
 
     app.use(express.static('doc'));
